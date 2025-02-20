ae9dd3b40ba827033cea07c343477ac736fd92e4
Muhammad Mehtab AHmed <mehtabahmed7777777@gmail.com>
2025-02-21 03:28:09 +0500

    cors fixed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 const corsOptions = {
-  origin: "http://localhost:5173", // Frontend origin
+  origin: "https://calcite-trial-frontend.onrender.com/", // Frontend origin
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "Cache-Control"],
